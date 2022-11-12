89618ad81dbace7c350e37a59f871170cd9f7b6f
Lova Juhlin <lova.juhlin@outlook.com>
2022-11-12 09:00:25 +0100

    webbapp-static

---
+++ assets/js/script6.js
@@ -0,0 +1,48 @@
+const checkLength = (element, minLength = 2, message) => {
+
+    if(message === undefined)
+        message = `Your ${element.target.id} must contain at least ${minLength} characters`
+
+
+    if(element.target.value.length < minLength) {
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMassage`).innerText = message
+        }
+    else { 
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMassage`).innerText = ""
+        
+ }
+    }
+
+const checkEmail = (element, message) => {
+    if(message === undefined)
+        message = `Your ${element.target.id} must be a valid e-mail adress`
+
+    if(!element.target.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ))
+    {
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMassage`).innerText = message
+
+    }
+    else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMassage`).innerText = ""
+        
+    }
+} 
+
+const validate = (e) => {
+ switch(e.target.type) {
+    case "text":
+        checkLength(e)
+        break;
+    case "email":
+        checkEmail(e)
+        break;
+    case "textarea":
+        checkLength(e, 5)
+        break;
+ }
+}
